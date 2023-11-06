7fc1c7e986032e19d35ef696ddd082eee54591c4
Paola2005 <137318847+Paola2005@users.noreply.github.com>
2023-11-06 12:08:11 -0500

    feat: :sparkles: Final

--- Domain/Interfaces/IUnitOfWork.cs
+++ Domain/Interfaces/IUnitOfWork.cs
@@ -9,6 +9,23 @@ namespace Domain.Interfaces
     {
         IRol Roles { get; }
         IUser Users { get; }
+        IBrand Brands { get; }
+        ICity Cities { get; }
+        IContactType ContactsTypes { get; }
+        ICountry Countries { get; }
+        IDocumentType DocumentsTypes { get; }
+        IInventory Inventories { get; }
+        IInventoryMovement InventoriesMovements { get; }
+        IInventoryMovementDetail InventoriesMovementsDetails { get; }
+        IInventoryMovementType InventoriesMovementsTypes { get; }
+        IPaymentMethod PaymentsMethods { get; }
+        IPersonContact PersonsContacts { get; }
+        IPresentation Presentations { get; }
+        IProduct Products { get; }
+        IState States { get; }
+        ITypePerson TypesPersons { get; }
+        IUbicationPerson UbicationsPersons { get; }
+        
 
         Task<int> SaveAsync();
     }
